8f34265ed9d86a3a5a5e49e30ffd1a3bf3df3de9
dorakkj <doravitoria77@gmail.com>
2024-10-18 08:42:40 -0300

    c

--- main.js
+++ main.js
@@ -5,15 +5,12 @@ cartao.className = 'cartao';
 cartao.innerHTML = `
 <div class="conteudo-cartao">
     <h3>categoria</h3>
-
     <div class="pergunta-cartao">
         <p>pergunta</p>
     </div>
-
     <div class="resposta-cartao">
         <p>resposta</p>
     </div>
-    
 </div>
 `
 container.appendChild(cartao);
