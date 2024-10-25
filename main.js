244a9025c2a6cda4510203c2b9b2867205cc858c
dorakkj <doravitoria77@gmail.com>
2024-10-25 11:22:11 -0300

    c

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ cartao.innerHTML = `
     </div>
 </div>
 `
+
+let respostaEstaVisivel= falsa;
+function viraCartao(){
+    respostaEstaVisivel=!respostaEstaVisivel;
+    cartao.classList.toggle('active', respostaEstaVisivel);
+}
+cartao.addEventListener('click',viraCartao);
+
 container.appendChild(cartao);
 }
 
